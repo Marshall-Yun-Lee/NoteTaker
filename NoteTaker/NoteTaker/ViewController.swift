b7921397d25e863052e1cd83a674e675e1fe7324
Marshall <gkslql456@gmail.com>
2020-08-30 22:41:31 -0700

    xcode setup and backend src moved

---
+++ NoteTaker/NoteTaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteTaker
+//
+//  Created by Marshall Lee on 2020-08-30.
+//  Copyright © 2020 Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
