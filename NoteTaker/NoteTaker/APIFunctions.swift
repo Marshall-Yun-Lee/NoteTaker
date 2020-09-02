fe4770f43a78be7375db7fad39cd48b52d5d371d
Marshall <gkslql456@gmail.com>
2020-09-02 12:43:01 -0700

    implementing note lists

---
+++ NoteTaker/NoteTaker/APIFunctions.swift
@@ -0,0 +1,32 @@
+//
+//  APIFunctions.swift
+//  NoteTaker
+//
+//  Created by Marshall Lee on 2020-09-02.
+//  Copyright © 2020 Marshall. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct Note {
+    var _id: String
+    var title: String
+    var date: String
+    var note: String
+}
+
+class APIFunctions {
+    
+    var delegate: DataDelegate?
+    static let functions = APIFunctions()
+    
+    func fetchNotes() {
+        AF.request("http://10.0.0.26:8080/fetch").response { response in
+            print(response.data!)
+            let data = String(data: response.data!, encoding: .utf8)
+            
+            self.delegate?.updateNotes(newArray: data)
+        }
+    }
+}
--- NoteTaker/NoteTaker/ViewController.swift
+++ NoteTaker/NoteTaker/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol DataDelegate {
+    func updateArray(newArray: String)
 }
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var notes = [Note]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+//        cell.textLabel?.text = notes[indexPath.row]
+        return cell
+    }
+    
+    
+    @IBOutlet weak var notesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        APIFunctions.functions.delegate = self
+        APIFunctions.functions.fetchNotes()
+        print(notes)
+        
+        notesTableView.delegate = self
+        notesTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: DataDelegate {
+    func updateArray(newArray: String) {
+        do {
+            notes = try JSONDecoder().decode([Note].self, from: newArray.data(using: .utf8)!)
+        } catch {
+            print("Failed to decode json")
+        }
+    }
+}
